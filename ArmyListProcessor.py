39f98a3652b99cc99eec6685113bd84ddc40a786
DESKTOP-L9FTQHA\keenan <keenanempire@gmail.com>
2026-03-13 20:13:14 -0700

    Fixed output writer from writing out the whole tuple, instead of just the original "row" that the tuple as made from. The tuple is there for easy sorting in python.

--- ArmyListProcessor.py
+++ ArmyListProcessor.py
@@ -33,15 +33,18 @@ new_list = []
 with open(INPUT_FILE_NAME, mode = "r", newline ="", encoding="utf-8") as file:
     csv_reader = csv.reader(file)
 
-    discard_re = re.compile(r"^(unit|\d+ |(\d*)x|leader|Categories|Rules|Abilities)|\b(?:{Move|don't|models|ranged weapons|melee weapons})\b|\A\Z", re.IGNORECASE)
+    # TODO: consider splitting up regex into "Header rows" and "Noise rows" patterns
+    discard_re = re.compile(r"^(unit|\d+ |(\d*)x|leader|categories|rules)|\b(?:move|don't|models|ranged weapons|melee weapons)\b|\A\Z", re.IGNORECASE)
 
     #options_re = re.compile(r"(\d*)x", re.IGNORECASE)
 
     #leader_re = re.compile(r"^leader", re.IGNORECASE)
 
     new_unit = False
+    abilities_section = False
     current_unit_name = "UnitFindingLogicBroken"
     current_unit_toughness = 0
+    current_unit_starting_index = 0
 
     for row in csv_reader:
 
@@ -54,27 +57,31 @@ with open(INPUT_FILE_NAME, mode = "r", newline ="", encoding="utf-8") as file:
             break
 
         if new_unit: # this is executed if the previous line was "Unit", which means the next line contains special information we want.
-            new_unit = False
             current_unit_name = row[0]
-            current_unit_toughness = int(row[2])
-            print(current_unit_toughness)
+            current_unit_toughness = int(row[2]) # this assumes every model inside the unit has the same toughness. Which is not always true for some factions. TODO: Fix unit toughness sameness assumption
+            #print(current_unit_toughness)
+            new_unit = False
         elif row[0].startswith("Unit"):
             new_unit = True
 
-        if row[1] == "" or match: # this is a row that contains units a leader can be attached to, and for now isn't interesting.
+        if match or row[1] == "": # row[1] being blank means it's a leadership block, and we discard it for now.
             continue
-
         else:
             new_list.append((current_unit_toughness, current_unit_name, row))
 
     #print(new_list)
 
-    sorted_list = sorted(new_list, key = lambda tup: (tup[0], tup[1]))
+    sorted_list = sorted(new_list, key = lambda x: (x[0], x[1])) # sort the tuples first by toughness/range of the unit, then by unit name
 
     #print(sorted_list)
 
+    #TODO: remove duplicate stat unit rows (infantry squads and their sargent who have the exact same stats)
+    #TODO: Move the abilities rows to the right of the stat block with a column of space, 8th cell or index 7.
+
     with open(OUTPUT_FILE_NAME, mode="w", newline="", encoding="utf-8") as out_file:
         out_writer = csv.writer(out_file)
-        out_writer.writerows(sorted_list)
+        for tup in sorted_list:
+            out_writer.writerow(tup[2])
+        #out_writer.writerows(sorted_list)
 
 
